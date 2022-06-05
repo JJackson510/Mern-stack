1b80936dbcbabea526c12d2f4e1ccb19370ca247
JJackson510 <jamiej.jj5@gmail.com>
2022-06-05 17:40:47 -0500

    add middleware

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -9,6 +9,11 @@ const getGoals = (req, res) => {
 //@route Post /api/goals
 //@access Private
 const setGoal = (req, res) => {
+
+    if(req.body.text) {
+        res.status(400)
+        throw new Error('Please add a text feild')
+    }
     res.status(200).json({message: 'Set goals'})
 }
 
